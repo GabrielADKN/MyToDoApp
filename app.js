027c5f7720de05d8b9be0c7cb37165a128224fb6
GabrielADKN <adoukonoubienheureux@gmail.com>
2023-10-13 13:25:24 -0400

    To Do App first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('#Myform');
+const MyInput = document.querySelector('input');
+const ul = document.querySelector('#listToDo');
+
+// Load todos from localStorage when the page loads
+const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
+// Function to save todos to localStorage
+function saveTodos() {
+    const todos = Array.from(ul.querySelectorAll('li')).map(li => li.firstChild.textContent);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const li = document.createElement('li');
+
+    const text = document.createElement('span'); // Separate element for text content
+    text.textContent = MyInput.value;
+    li.appendChild(text);
+    li.classList.add('new', 'todo');
+
+    const addBtn = document.createElement('button');
+    addBtn.textContent = 'Delete';
+    addBtn.style.backgroundColor = 'red';
+    li.appendChild(addBtn);
+
+    ul.appendChild(li);
+    MyInput.value = '';
+
+    // addBtn.addEventListener('click', () => {
+    //     li.remove();
+    //     saveTodos();
+    // });
+
+    li.addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON'){
+            e.target.parentElement.remove();
+        } else {
+            e.target.classList.toggle('completed');
+        }
+    });
+    saveTodos(); // Save todos after adding a new todo
+});
+
+// Load saved todos onto the page
+savedTodos.forEach(todoText => {
+    const li = document.createElement('li');
+
+    const text = document.createElement('span'); // Separate element for text content
+    text.textContent = todoText;
+    li.appendChild(text);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.style.backgroundColor = 'red';
+    li.appendChild(deleteBtn);
+
+    ul.appendChild(li);
+
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+        saveTodos();
+    });
+});
